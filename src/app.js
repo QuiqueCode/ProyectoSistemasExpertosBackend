c997ad704502c7f8f88d8196212cabe45d871bfe
Kendel~ <kendell21am@gmail.com>
2024-01-15 20:36:27 -0600

    recommendations v3

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors'; 
 import routerTours from './routes/rutas.js';
+import recommendations from './routes/rutas.js';
 
 const app = express();
 
@@ -9,7 +10,7 @@ app.use(cors());
 
 // Middleware que permite responder y recibir objetos JSON
 app.use(express.json());
-app.use('/api', routerTours);
+app.use('/api', routerTours,recommendations);
 
 // Manejar 404 - Endpoint no encontrado
 app.use((req, res, next) => {
--- src/controllers/recommendations.js
+++ src/controllers/recommendations.js
@@ -3,7 +3,7 @@ import { pool } from "../database.js";
 
 export const recommendations = async (req, res) => {
     try {
-        const [rows] = await pool.query("CALL getRecomendation(?);")
+        const [rows] = await pool.query("CALL getRecomendation(?);", [req.query.id])
         if (rows[0].length <= 0) {
             return res.status(404).json({ message: "Recomendacion no encontrada." })
         }
