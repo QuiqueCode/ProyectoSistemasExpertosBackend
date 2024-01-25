8a90082b395292391c3b5167cffe2e867a8f2c97
Kendel~ <kendell21am@gmail.com>
2024-01-25 13:33:01 -0600

    edit

--- src/controllers/recommendations.js
+++ src/controllers/recommendations.js
@@ -1,34 +1,38 @@
 import { pool } from "../database.js";
 
 //Returns the preferences response from the database
-async function preferences(idUser){
-    return await pool.query("CALL getPreference(?);", [idUser]);
+async function preferences(idUser) {
+  return await pool.query("CALL getPreference(?);", [idUser]);
 }
 
 //Returns the recommendations response from the database
-async function recommendations(idUser){
-    return await pool.query("CALL getRecomendation(?);", [idUser]);
+async function recommendations(idUser) {
+  return await pool.query("CALL getRecomendation(?);", [idUser]);
 }
 
 export const methodRecommend = async (req, res) => {
-    try {
-        const id = req.query.id;
-        //get preference value
-        const [[[preferencesRows]]] = await preferences(id)
-        //get recommendation value
-        const [[[recomendationRows]]] = await recommendations(id)
-        let rows;
+  try {
+    const id = req.query.id;
+    //get preference value
+    const [[[preferencesRows]]] = await preferences(id);
 
-        if(preferencesRows.preference != 0){
-            [[rows]] = await pool.query("CALL getToursForRecomendation(?);", [preferencesRows.preference]);
-        }else{
-            [[rows]] = await pool.query("CALL getToursForRecomendation(?);", [recomendationRows.result]);
-        }
+    let rows;
 
-        return res.json(rows);
-
-    } catch (error) {
-        console.error(error);
-        return res.status(500);
+    if (preferencesRows.preference != 0) {
+      [[rows]] = await pool.query("CALL getToursForRecomendation(?);", [
+        preferencesRows.preference,
+      ]);
+    } else {
+      //get recommendation value
+      const [[[recomendationRows]]] = await recommendations(id);
+      [[rows]] = await pool.query("CALL getToursForRecomendation(?);", [
+        recomendationRows.result,
+      ]);
     }
-}
+
+    return res.json(rows);
+  } catch (error) {
+    console.error(error);
+    return res.status(500);
+  }
+};
