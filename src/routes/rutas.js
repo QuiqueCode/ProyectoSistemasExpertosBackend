7857538971de994886cb4a8b46e7e393e27cae30
Kendel~ <kendell21am@gmail.com>
2024-01-26 15:39:54 -0600

    recommendation slider

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { tours } from "../controllers/tours.js";
-import { createUser, login } from "../controllers/users.js";
+import { createUser, login, changePreference } from "../controllers/users.js";
 import { methodRecommend } from "../controllers/recommendations.js";
 
 const routerTours=Router();
@@ -8,4 +8,5 @@ routerTours.get('/recommendations',methodRecommend);
 routerTours.get('/tours',tours);
 routerTours.post('/users', createUser);
 routerTours.post('/login', login);
+routerTours.patch('/updatepreference', changePreference);
 export default routerTours;
