5431168d550a9b699008250fe55605bf4d9ee4b9
diegotech14 <diegoduarte8343@gmail.com>
2024-01-26 08:32:53 -0600

    clickmethod

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { tours } from "../controllers/tours.js";
 import { createUser, login } from "../controllers/users.js";
-import { methodRecommend } from "../controllers/recommendations.js";
+import { counterTours, methodRecommend } from "../controllers/recommendations.js";
 
 const routerTours=Router();
 routerTours.get('/recommendations',methodRecommend);
 routerTours.get('/tours',tours);
 routerTours.post('/users', createUser);
 routerTours.post('/login', login);
+routerTours.patch('/counter', counterTours);
 export default routerTours;
